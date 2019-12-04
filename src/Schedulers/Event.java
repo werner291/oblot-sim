49d6092fb1cb185dcd13ce329694f00aacd7294d
ifidefix <tompeters895@gmail.com>
2019-12-04 20:11:06 +0100

    Implemented a scheduler that reads from a file input

--- src/Schedulers/Event.java
+++ src/Schedulers/Event.java
@@ -1,5 +1,7 @@
 package Schedulers;
 
+import Algorithms.Robot;
+
 /**
  * An event of a robot. Could be either start compute, start moving
  */
@@ -15,13 +17,25 @@ public class Event {
     public double timeStamp;
 
     /**
+     * The {@link Robot} this event belongs to.
+     */
+    public Robot r;
+
+    /**
      * Creates a new event
      * @param type the type of event
      * @param timeStamp the timestamp of the event
+     * @param r the {@link Robot} this event belongs to
      */
-    public Event(EventType type, double timeStamp) {
+    public Event(EventType type, double timeStamp, Robot r) {
         this.type = type;
         this.timeStamp = timeStamp;
+        this.r = r;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Event: %s, %s, %f", type, r, timeStamp);
     }
 
     /**
@@ -30,6 +44,10 @@ public class Event {
     public enum EventType {
         START_COMPUTE,
         START_MOVING,
-        END_MOVING
+        END_MOVING;
+
+        public static EventType next(EventType e) {
+            return EventType.values()[(e.ordinal() + 1) % EventType.values().length];
+        }
     }
 }
