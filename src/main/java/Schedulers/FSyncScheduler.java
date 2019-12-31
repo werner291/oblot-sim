24d51951fa44bfd6a6656cf860240604c41ca18b
bartiimus <bartiimus@users.noreply.github.com>
2019-12-31 13:30:23 +0100

    changed the fsync and sync scheduler to be stateless

--- src/main/java/Schedulers/FSyncScheduler.java
+++ src/main/java/Schedulers/FSyncScheduler.java
@@ -1,8 +1,10 @@
 package Schedulers;
 
 import Simulator.Robot;
+import Simulator.State;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FSyncScheduler extends SyncScheduler {
@@ -23,22 +25,60 @@ public class FSyncScheduler extends SyncScheduler {
     }
 
     @Override
-    void computeNextEvent(Robot[] robots, double t) {
-        double computeTime = minComputeTime + (maxComputeTime - minMoveTime) * random.nextDouble();
-        double moveTime = minMoveTime + (maxMoveTime - minMoveTime) * random.nextDouble();
-
-        List<Event> startComputeEvents = new ArrayList<>();
-        List<Event> startMoveEvents = new ArrayList<>();
-        List<Event> endMoveEvents = new ArrayList<>();
-
+    public List<Event> getNextEvent(Robot[] robots, double t) {
+        EventType nextType = null;
+        State currentState = null;
         for (Robot robot: robots) {
-            startComputeEvents.add(new Event(EventType.START_COMPUTE, t, robot));
-            startMoveEvents.add(new Event(EventType.START_MOVING, t + computeTime, robot));
-            endMoveEvents.add(new Event(EventType.END_MOVING, t + computeTime + moveTime, robot));
+            if (nextType == null) {
+                switch (robot.state) {
+                    case SLEEPING:
+                        nextType = EventType.START_COMPUTE;
+                        break;
+                    case COMPUTING:
+                        nextType = EventType.START_MOVING;
+                        break;
+                    case MOVING:
+                        nextType = EventType.END_MOVING;
+                        break;
+                }
+                currentState = robot.state;
+            }else if (currentState != robot.state) {
+                throw new IllegalStateException("not all the robots have the same state");
+            }
         }
-        events.add(startComputeEvents);
-        events.add(startMoveEvents);
-        events.add(endMoveEvents);
-        timestampLastEvent = t + computeTime + moveTime;
+
+        List<Event> events = new ArrayList<>();
+        switch (nextType) {
+            case START_COMPUTE:
+                double computeStart = t + random.nextDouble();
+                for (Robot robot: robots) {
+                    events.add(new Event(nextType, computeStart, robot));
+                }
+                break;
+            case START_MOVING:
+                double computeTime = minComputeTime + (maxComputeTime - minMoveTime) * random.nextDouble();
+
+                for (Robot robot: robots) {
+                    if (robot.state == State.COMPUTING) {
+                        events.add(new Event(nextType, t + computeTime, robot));
+                    }
+                }
+                break;
+            case END_MOVING:
+                double endTime;
+                if (nextEndMoving > t) {
+                    endTime = nextEndMoving;
+                }else {
+                    endTime = t + minMoveTime + (maxMoveTime - minMoveTime) * random.nextDouble();;
+                }
+
+                for (Robot robot: robots) {
+                    if (robot.state == State.MOVING) {
+                        events.add(new Event(nextType, endTime, robot));
+                    }
+                }
+                break;
+        }
+        return events;
     }
 }
--- src/main/java/Schedulers/SyncScheduler.java
+++ src/main/java/Schedulers/SyncScheduler.java
@@ -1,6 +1,7 @@
 package Schedulers;
 
 import Simulator.Robot;
+import Simulator.State;
 
 import java.util.*;
 ;
@@ -11,8 +12,8 @@ public class SyncScheduler extends Scheduler {
     double maxComputeTime;
     double minMoveTime;
     double maxMoveTime;
+    double nextEndMoving = 0;
 
-    List<List<Event>> events;
     double timestampLastEvent = 0;
 
     Random random;
@@ -36,134 +37,82 @@ public class SyncScheduler extends Scheduler {
         this.minMoveTime = minMoveTime;
         this.maxMoveTime = maxMoveTime;
 
-        events = new ArrayList<>();
+
         random = new Random();
-    }
-
-    void computeNextEvent(Robot[] robots, double t) {
-        int NROFRobots = random.nextInt(robots.length-1) + 1;
-        List<Robot> clonedRobots = new ArrayList<>(Arrays.asList(robots));
-
-        List<Robot> chosenRobots = new ArrayList<>();
-
-        for (int i = 0; i < NROFRobots; i++) {
-            int chosenRobot = random.nextInt(clonedRobots.size());
-            chosenRobots.add(clonedRobots.get(chosenRobot));
-            clonedRobots.remove(chosenRobot);
-        }
-
-        double computeTime = minComputeTime + (maxComputeTime - minMoveTime) * random.nextDouble();
-        double moveTime = minMoveTime + (maxMoveTime - minMoveTime) * random.nextDouble();
-
-        List<Event> startComputeEvents = new ArrayList<>();
-        List<Event> startMoveEvents = new ArrayList<>();
-        List<Event> endMoveEvents = new ArrayList<>();
-
-        for (Robot robot: chosenRobots) {
-            startComputeEvents.add(new Event(EventType.START_COMPUTE, t, robot));
-            startMoveEvents.add(new Event(EventType.START_MOVING, t + computeTime, robot));
-            endMoveEvents.add(new Event(EventType.END_MOVING, t + computeTime + moveTime, robot));
-        }
-        events.add(startComputeEvents);
-        events.add(startMoveEvents);
-        events.add(endMoveEvents);
-        timestampLastEvent = t + computeTime + moveTime;
-
-    }
-
-    /**
-     * Finds the index i in the event array such that event[i].timestamp > t && event[i-1].timestamp <= t
-     * It returns events.length if event[events.length-1].timestamp <= t
-     * @param t the timestamp to look for
-     * @return the index of the correct event
-     */
-    private int binarySearch(double t) {
-        return binarySearch(0, events.size() - 1, t);
-    }
-
-    /**
-     * Recursive implementation of binary search
-     * @param t the timestamp to look for
-     * @return the index of the correct event
-     */
-    private int binarySearch(int l, int r, double t)
-    {
-        if (r >= l) {
-            int mid = l + (r - l) / 2;
-
-            // check the special case at the beginning
-            if (mid == 0 && events.get(0).get(0).t > t) {
-                return 0;
-            }
-
-            // check the special cases at the end
-            if (mid == events.size() - 2 && events.get(mid+1).get(0).t <= t) { // -2 because mid calculation will always be rounded down
-                return events.size();
-            }
-            if (events.get(mid).get(0).t <= t && events.get(mid+1).get(0).t > t) {
-                return mid+1;
-            }
-
-            // recurse on left or right part of the array
-            if (events.get(mid).get(0).t > t) {
-                return binarySearch(l, mid, t);
-            } else {
-                return binarySearch(mid, r, t);
-            }
-        }
-        // Whenever this happens, there is a bug in the binary search.
-        // It should find the answer in all cases before r < l
-        throw new IllegalStateException("r < l, this should never happen.");
     }
 
     @Override
     public List<Event> getNextEvent(Robot[] robots, double t) {
-        while (t >= timestampLastEvent) {
-            computeNextEvent(robots, timestampLastEvent );
+        EventType nextType = null;
+        for (Robot robot: robots) {
+            if (nextType == null || nextType == EventType.START_COMPUTE) {
+                switch (robot.state) {
+                    case SLEEPING:
+                        nextType = EventType.START_COMPUTE;
+                        break;
+                    case COMPUTING:
+                        nextType = EventType.START_MOVING;
+                        break;
+                    case MOVING:
+                        nextType = EventType.END_MOVING;
+                        break;
+                }
+            }else if (nextType == EventType.START_MOVING && robot.state == State.MOVING) {
+                throw new IllegalStateException("One robot is moving and one is computing which is not possible in Sync");
+            }else if (nextType == EventType.END_MOVING && robot.state == State.COMPUTING) {
+                throw new IllegalStateException("One robot is moving and one is computing which is not possible in Sync");
+            }
         }
-        return events.get(binarySearch(t));
+
+        List<Event> events = new ArrayList<>();
+        switch (nextType) {
+            case START_COMPUTE:
+                int NROFRobots = random.nextInt(robots.length-1) + 1;
+                List<Robot> clonedRobots = new ArrayList<>(Arrays.asList(robots));
+
+                List<Robot> chosenRobots = new ArrayList<>();
+
+                for (int i = 0; i < NROFRobots; i++) {
+                    int chosenRobot = random.nextInt(clonedRobots.size());
+                    chosenRobots.add(clonedRobots.get(chosenRobot));
+                    clonedRobots.remove(chosenRobot);
+                }
+                double computeStart = t + random.nextDouble();
+                for (Robot robot: chosenRobots) {
+                    events.add(new Event(nextType, computeStart, robot));
+                }
+                break;
+            case START_MOVING:
+                double computeTime = minComputeTime + (maxComputeTime - minMoveTime) * random.nextDouble();
+
+                for (Robot robot: robots) {
+                    if (robot.state == State.COMPUTING) {
+                        events.add(new Event(nextType, t + computeTime, robot));
+                    }
+                }
+                break;
+            case END_MOVING:
+                double endTime;
+                if (nextEndMoving > t) {
+                    endTime = nextEndMoving;
+                }else {
+                    endTime = t + minMoveTime + (maxMoveTime - minMoveTime) * random.nextDouble();;
+                }
+
+                for (Robot robot: robots) {
+                    if (robot.state == State.MOVING) {
+                        events.add(new Event(nextType, endTime, robot));
+                    }
+                }
+                break;
+        }
+        return events;
     }
 
     @Override
     public void addEvent(Event e) {
-        int index = binarySearch(e.t);
-        int indexSameRobotAfter = -1;
-        for (int i = index; i < events.size(); i++) {
-            for (Event event: events.get(i)) {
-                if (event.r == e.r){
-                    indexSameRobotAfter = i;
-                    break;
-                }
-            }
+        if (e.type == EventType.END_MOVING && e.t > nextEndMoving) {
+            nextEndMoving = e.t;
         }
-        int indexSameRobotBefore = -1; // or with the same timestamp
-        for (int i = index - 1; i >= 0; i--) {
-            for (Event event: events.get(i)) {
-                if (event.r == e.r) {
-                    indexSameRobotBefore = i;
-                    break;
-                }
-            }
-        }
-
-
-        // check if the event falls strictly in between
-        if (indexSameRobotBefore != -1 && events.get(indexSameRobotBefore).get(0).t < e.t) {
-            // if it does not break the chain of events (START_COMPUTE, START_MOVE, END_MOVE), insert it
-            // it should be the eventType after the previous for the same robot, but it should be the same type as the one
-            // it will be switched for.
-            if (indexSameRobotAfter != -1 && EventType.next(events.get(indexSameRobotBefore).get(0).type) == e.type && e.type == events.get(indexSameRobotAfter).get(0).type) { // it is a correct insertion
-                // insert e and remove the event at indexSameRobotAfter. Shift everything in between
-                if (indexSameRobotAfter - index >= 0) {
-                    System.arraycopy(events, index, events, index + 1, indexSameRobotAfter - index);
-                }
-                ArrayList<Event> newEvent = new ArrayList<>();
-                newEvent.add(e);
-                events.add(index, newEvent);
-            }
-        }
-        // in case the event is not strictly in between the other events
-        // for the same robot, but instead it has the same timestamp as an already defined event in the list,
-        // this will always break the natural chain of events
     }
 }
