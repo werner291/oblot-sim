1e9b67060d54ff511c69b7dcd94d218ac6149c7a
ifidefix <tompeters895@gmail.com>
2020-01-05 13:58:19 +0100

    Fixed interruptable

--- src/main/java/Simulator/Simulator.java
+++ src/main/java/Simulator/Simulator.java
@@ -105,10 +105,12 @@ public class Simulator {
                     }
                     e.r.state = State.MOVING;
                     goal = goals[Arrays.asList(robots).indexOf(e.r)];
-                    // add next end_move to the scheduler
-                    double endTime = Interpolate.getEndTime(e.r.pos, currentTime, goal, e.r.speed);
-                    Event proposed_end_move = new Event(EventType.END_MOVING, endTime, e.r);
-                    scheduler.addEvent(proposed_end_move);
+                    if (!config.interuptable) {
+                        // add next end_move to the scheduler
+                        double endTime = Interpolate.getEndTime(e.r.pos, currentTime, goal, e.r.speed);
+                        Event proposed_end_move = new Event(EventType.END_MOVING, endTime, e.r);
+                        scheduler.addEvent(proposed_end_move);
+                    }
                     break;
                 case END_MOVING:
                     if (e.r.state != State.MOVING) {
